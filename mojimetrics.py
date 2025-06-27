5a6b0c7ec12204ae17033ee5eb2978025ac211b5
dblind-submission <dblindsubmission@gmail.com>
2025-06-27 17:04:20 +1000

    Update

--- mojimetrics.py
+++ mojimetrics.py
@@ -6,30 +6,7 @@ import pandas as pd
 from sklearn import metrics
 from sklearn.metrics import precision_recall_curve, average_precision_score
 
-def PlotROCCurve(probs,y_test_roc, ci= 95, random_seed=0):
-    
-    fpr, tpr, threshold = metrics.roc_curve(y_test_roc,probs)
-    roc_auc = metrics.auc(fpr, tpr)
-    average_precision = average_precision_score(y_test_roc, probs)
-    a=np.sqrt(np.square(fpr-0)+np.square(tpr-1)).argmin()
-    sensitivity = tpr[a]
-    specificity = 1-fpr[a]
-    threshold = threshold[a]
-    # print("AUC:",roc_auc)
-    # print("AUPRC:", average_precision)
-    # print("Sensitivity:",sensitivity)
-    # print("Specificity:",specificity)
-    # print("Score thresold:",threshold)
-    lower_auroc, upper_auroc, std_auroc, lower_ap, upper_ap, std_ap, lower_sensitivity, upper_sensitivity, std_sensitivity, lower_specificity, upper_specificity, std_specificity = auc_with_ci(probs,y_test_roc, lower = (100-ci)/2, upper = 100-(100-ci)/2, n_bootstraps=20, rng_seed=random_seed)
-    # print("AUC CI:", lower_auroc, upper_auroc)
-    # print("AUC Standard Deviation:", std_auroc)
-    # print("AUPRC CI:", lower_ap, upper_ap)
-    # print("AUPRC Standard Deviation:", std_ap)
-    # print("Sensitivity CI:", lower_sensitivity, upper_sensitivity)
-    # print("Sensitivity Standard Deviation:", std_sensitivity)
-    # print("Specificity CI:", lower_specificity, upper_specificity)
-    # print("Specificity Standard Deviation:", std_specificity)
-    return roc_auc, average_precision, sensitivity, specificity, threshold
+
 
 
 def auc_with_ci(probs,y_test_roc, lower = 2.5, upper = 97.5, n_bootstraps=200, rng_seed=10):
